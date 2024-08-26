25844afc2f4836464bebb4cbd22bdfc9bdbd4826
chendaliang <piaoyaocdl@163.com>
2024-08-27 01:15:14 +0800

    1

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+﻿import { operator_EQ as dd, operator_GEQ, operator_LEQ } from "./operations_research/GFunc";
+
+export namespace operations_research
+{
+    export const operator_EQ = dd;
+    operator_GEQ;
+    operator_LEQ;
+}
