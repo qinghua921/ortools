767c5ee74490eb0124aa1b9b7e2cff9e202d5f3f
chendaliang <piaoyaocdl@163.com>
2024-09-11 14:47:22 +0800

    1

--- src/operations_research/sat/GCpModelProto.hpp
+++ src/operations_research/sat/GCpModelProto.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-
 #include "commonheader.hpp"
 #include "ortools/sat/cp_model.h"
 
@@ -8,19 +7,9 @@ namespace operations_research
 {
 namespace sat
 {
-    class GCpModelProto : public Napi::ObjectWrap< GCpModelProto >
-    {
-    public:
-        static inline Napi::FunctionReference constructor;
-        CpModelProto*                  pCpModelProto = nullptr;
-        GCpModelProto( const Napi::CallbackInfo& info );
-        ~GCpModelProto();
-        static Napi::Object Init( Napi::Env env, Napi::Object exports );
-    };
+    WrapOrToolsClass( CpModelProto );
 };  // namespace sat
 };  // namespace operations_research
-
-
 
 inline operations_research::sat::GCpModelProto::GCpModelProto( const Napi::CallbackInfo& info )
     : Napi::ObjectWrap< GCpModelProto >( info )
@@ -28,19 +17,14 @@ inline operations_research::sat::GCpModelProto::GCpModelProto( const Napi::Callb
     if ( info.Length() == 1 && info[ 0 ].IsExternal() )
     {
         auto external = info[ 0 ].As< Napi::External< CpModelProto > >();
-        pCpModelProto = dynamic_cast< CpModelProto* >( external.Data() );
-        if ( pCpModelProto != nullptr ) return;
+        shared_ptr    = std::shared_ptr< CpModelProto >( external.Data() );
+        return;
     }
 
     ThrowJsError( operations_research::GCpModelProto::GCpModelProto : Invalid argument );
 }
 
-inline operations_research::sat::GCpModelProto::~GCpModelProto()
-{
-    delete pCpModelProto;
-}
-
-inline Napi::Object operations_research::sat::GCpModelProto::Init( Napi::Env env, Napi::Object exports )
+inline void operations_research::sat::GCpModelProto::Init( Napi::Env env, Napi::Object exports )
 {
     Napi::HandleScope scope( env );
     Napi::Function    func = DefineClass(
@@ -49,5 +33,4 @@ inline Napi::Object operations_research::sat::GCpModelProto::Init( Napi::Env env
     constructor = Napi::Persistent( func );
     constructor.SuppressDestruct();
     exports.Set( Napi::String::New( env, "CpModelProto" ), func );
-    return exports;
 }
