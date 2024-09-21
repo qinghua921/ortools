500a30494c0f659b8ef545a75e5cc44f16f174c4
chendaliang <piaoyaocdl@163.com>
2024-09-21 12:28:38 +0800

    1

--- src/index.cpp
+++ src/index.cpp
@@ -5,6 +5,9 @@
 #include "ortools/base/logging.h"
 #include "ortools/init/init.h"
 
+#include "operations_research/MPSolver.hpp"
+#include "operations_research/MPVariable.hpp"
+
 Napi::Object Init( Napi::Env env, Napi::Object exports )
 {
     google::InitGoogleLogging( "ortools_binding" );
@@ -13,6 +16,8 @@ Napi::Object Init( Napi::Env env, Napi::Object exports )
     Napi::HandleScope scope( env );
 
     auto operations_research = Napi::Object::New( env );
+    operations_research::GMPSolver::Init( env, operations_research );
+    operations_research::GMPVariable::Init( env, operations_research );
     exports.Set( "operations_research", operations_research );
 
     return exports;
---
+++ src/operations_research/MPSolver.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+#include "napi.h"
+#include "ortools/linear_solver/linear_solver.h"
+#include "MPVariable.hpp"
+
+namespace operations_research
+{
+
+class GMPSolver : public Napi::ObjectWrap< GMPSolver >
+{
+public:
+    static inline Napi::FunctionReference constructor;
+    MPSolver*                             pMPSolver = nullptr;
+
+    GMPSolver( const Napi::CallbackInfo& info )
+        : Napi::ObjectWrap< GMPSolver >( info )
+    {
+        Napi::Env env = info.Env();
+
+        if ( info.Length() == 1 && info[ 0 ].IsExternal() )
+        {
+            auto external = info[ 0 ].As< Napi::External< MPSolver > >();
+            pMPSolver     = dynamic_cast< MPSolver* >( external.Data() );
+            if ( pMPSolver ) return;
+        }
+
+        Napi::TypeError::New( env, "operations_research::GMPSolver::GMPSolver : Invalid arguments" ).ThrowAsJavaScriptException();
+    };
+
+    ~GMPSolver()
+    {
+        if ( pMPSolver ) delete pMPSolver;
+    };
+
+    static Napi::Object Init( Napi::Env env, Napi::Object exports )
+    {
+        Napi::HandleScope scope( env );
+        Napi::Function    func = DefineClass(
+            env, "MPSolver",
+            {
+                StaticMethod( "CreateSolver", &GMPSolver::CreateSolver ),
+                InstanceMethod( "MakeBoolVar", &GMPSolver::MakeBoolVar ),
+            } );
+        constructor = Napi::Persistent( func );
+        constructor.SuppressDestruct();
+        exports.Set( Napi::String::New( env, "MPSolver" ), func );
+        return exports;
+    };
+
+    // MPVariable* MakeBoolVar( const std::string& name );
+    Napi::Value MakeBoolVar( const Napi::CallbackInfo& info )
+    {
+        Napi::Env         env = info.Env();
+        Napi::HandleScope scope( env );
+
+        if ( info.Length() == 1 && info[ 0 ].IsString() )
+        {
+            std::string name     = info[ 0 ].As< Napi::String >().Utf8Value();
+            auto        pVar     = pMPSolver->MakeBoolVar( name );
+            auto        external = Napi::External< MPVariable >::New( env, pVar );
+            return GMPVariable::constructor.New( { external } );
+        }
+
+        Napi::TypeError::New( env, "operations_research::GMPSolver::MakeBoolVar : Invalid arguments" ).ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    // static MPSolver* CreateSolver( const std::string& solver_id );
+    static Napi::Value CreateSolver( const Napi::CallbackInfo& info )
+    {
+        Napi::Env         env = info.Env();
+        Napi::HandleScope scope( env );
+
+        if ( info.Length() == 1 && info[ 0 ].IsString() )
+        {
+            std::string solver_id = info[ 0 ].As< Napi::String >().Utf8Value();
+            auto        pMPSolver = MPSolver::CreateSolver( solver_id );
+            auto        external  = Napi::External< MPSolver >::New( env, pMPSolver );
+            return GMPSolver::constructor.New( { external } );
+        }
+
+        Napi::TypeError::New( env, "operations_research::GMPSolver::CreateSolver : Invalid arguments" ).ThrowAsJavaScriptException();
+        return env.Null();
+    }
+};
+
+};  // namespace operations_research
