9d3bbf7776855faa5d49524e90e6d3e65042fe94
chendaliang <piaoyaocdl@163.com>
2024-09-24 01:04:44 +0800

    0

---
+++ src/operations_research/packing/A.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "napi.h"
+
+namespace operations_research
+{
+namespace graph
+{
+    class GDemo : public Napi::ObjectWrap< GDemo >
+    {
+    public:
+        static inline Napi::FunctionReference constructor;
+        Demo*                                 pDemo = nullptr;
+
+        GDemo( const Napi::CallbackInfo& info )
+            : Napi::ObjectWrap< GDemo >( info )
+        {
+            Napi::Env env = info.Env();
+
+            if ( info.Length() == 1 && info[ 0 ].IsExternal() )
+            {
+                auto external = info[ 0 ].As< Napi::External< Demo > >();
+                pDemo         = dynamic_cast< Demo* >( external.Data() );
+                if ( pDemo ) return;
+            }
+
+            Napi::TypeError::New( env, "operations_research::GDemo::GDemo : Invalid arguments" ).ThrowAsJavaScriptException();
+        };
+
+        // TODO delete pDemo or not ?
+        // ~GDemo()
+        // {
+        //     if ( pDemo ) delete pDemo;
+        // };
+
+        static Napi::Object Init( Napi::Env env, Napi::Object exports )
+        {
+            Napi::HandleScope scope( env );
+            Napi::Function    func = DefineClass(
+                env, "Demo",
+                {} );
+            constructor = Napi::Persistent( func );
+            constructor.SuppressDestruct();
+            exports.Set( Napi::String::New( env, "Demo" ), func );
+            return exports;
+        };
+    };
+}  // namespace graph
+
+};  // namespace operations_research
