3e2074907dcb3a2eff11721aefa3775eca5e6064
chendaliang <piaoyaocdl@163.com>
2024-09-30 02:38:56 +0800

    0

--- gen/gen.ts
+++ gen/gen.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import YAML from 'yaml'
 import { CharStream, CommonTokenStream } from "antlr4ng";
 import { CPP14Lexer } from "./.antlr/CPP14Lexer";
-import { ClassHeadContext, ClassSpecifierContext, CPP14Parser, NamespaceDefinitionContext } from "./.antlr/CPP14Parser";
+import { AccessSpecifierContext, ClassSpecifierContext, CPP14Parser, EnumSpecifierContext, FunctionDefinitionContext, MemberdeclarationContext, MemberSpecificationContext, NamespaceDefinitionContext } from "./.antlr/CPP14Parser";
 import { CPP14ParserVisitor } from "./.antlr/CPP14ParserVisitor";
 
 interface GenConfig
@@ -11,78 +11,137 @@ interface GenConfig
     namespaces: string[];
 }
 
-class ClassInfo
+interface CPPInfo
 {
-    name: string;
-    namespace: string;
-
-    constructor(name: string, namespace: string)
-    {
-        this.name = name;
-        this.namespace = namespace;
-    }
+    type: 'NamespaceDefinition' | 'ClassSpecifier' | 'EnumSpecifier' | 'FunctionDefinition';
 }
 
-class NamespaceInfo
+interface FunctionDefinitionInfo extends CPPInfo
 {
+    type: 'FunctionDefinition';
     name: string;
-    classes: Map<string, ClassInfo> = new Map();
-
-    constructor(name: string)
-    {
-        this.name = name;
-    }
 }
+
+interface ClassSpecifierInfo extends CPPInfo
+{
+    type: 'ClassSpecifier';
+    name: string;
+}
+
+interface NamespaceDefinitionInfo extends CPPInfo
+{
+    type: 'NamespaceDefinition';
+    name: string;
+}
+
+interface EnumSpecifierInfo extends CPPInfo
+{
+    type: 'EnumSpecifier';
+    name: string;
+}
+
+
 
 class Info
 {
     gen_config!: GenConfig;
-    namespace_info: Map<string, NamespaceInfo> = new Map();
-    current_namespace: string = "";
+    cpp_info: { [path: string]: CPPInfo } = {};
+
+    current_path: string = "";
+
+    push_path(path: string | null | undefined)
+    {
+        this.current_path += "/" + path;
+    }
+
+    pop_path()
+    {
+        this.current_path = this.current_path.substring(0, this.current_path.lastIndexOf('/'));
+    }
 }
 
 const info = new Info();
 
 class OrToolsVisit extends CPP14ParserVisitor<void>
 {
-    visitNamespaceDefinition = (ctx: NamespaceDefinitionContext) =>
+    visitFunctionDefinition = (ctx: FunctionDefinitionContext) =>
     {
-        let identifier = ctx.Identifier();
-        if (!identifier)
-        {
-            this.visitChildren(ctx);
-            return;
-        }
-        if (!info.gen_config.namespaces.includes(identifier.getText()))
-        {
-            this.visitChildren(ctx);
-            return;
-        }
+        const function_name = ctx.declarator().pointerDeclarator()?.noPointerDeclarator().attributeSpecifierSeq()
+        console.log(function_name);
+        
+        // info.push_path(function_name);
 
-        info.current_namespace = identifier.getText();
-        if (!info.namespace_info.has(info.current_namespace))
+        // info.cpp_info[info.current_path] = {
+        //     type: 'FunctionDefinition'
+        // } as CPPInfo;
+
+        // this.visitChildren(ctx);
+
+        // info.pop_path();
+    }
+
+    visitMemberSpecification = (ctx: MemberSpecificationContext) =>
+    {
+        let public_ = false;
+        for (const member of ctx.children)
         {
-            info.namespace_info.set(info.current_namespace, new NamespaceInfo(info.current_namespace));
+            if (member instanceof AccessSpecifierContext)
+                public_ = !!(member as AccessSpecifierContext).Public()
+
+            if (!public_) continue;
+
+            this.visitChildren(member)
         }
+    }
+
+    visitEnumSpecifier = (ctx: EnumSpecifierContext) =>
+    {
+        const enum_name = ctx.enumHead().Identifier()?.getText() ?? ctx.enumHead().nestedNameSpecifier()?.getText()
+
+        info.push_path(enum_name);
+
+        info.cpp_info[info.current_path] = {
+            type: 'EnumSpecifier',
+            name: enum_name
+        } as EnumSpecifierInfo;
 
         this.visitChildren(ctx);
+
+        info.pop_path();
+    }
+
+    visitNamespaceDefinition = (ctx: NamespaceDefinitionContext) =>
+    {
+        const namespace_name = ctx.Identifier()?.getText() ?? ctx.originalNamespaceName()?.getText()
+
+        info.push_path(namespace_name);
+
+        info.cpp_info[info.current_path] = {
+            type: 'NamespaceDefinition',
+            name: namespace_name
+        } as NamespaceDefinitionInfo;
+
+        this.visitChildren(ctx);
+
+        info.pop_path();
     };
 
     visitClassSpecifier = (ctx: ClassSpecifierContext) =>
     {
-        let className = ctx.classHead()?.classHeadName()?.className();
+        const class_name = ctx.classHead().classHeadName()?.className().getText()
 
-        if (!className)
-        {
-            this.visitChildren(ctx);
-            return;
-        }
+        info.push_path(class_name);
 
-        let classInfo = new ClassInfo(className.getText(), info.current_namespace);
-        info.namespace_info.get(info.current_namespace)!.classes.set(classInfo.name, classInfo);
+        info.cpp_info[info.current_path] = {
+            type: 'ClassSpecifier',
+            name: class_name
+        } as ClassSpecifierInfo;
 
         this.visitChildren(ctx);
+
+        info.pop_path();
     }
+
 }
 
 /***********************************************************************************
@@ -102,8 +161,8 @@ function main()
         const lexer = new CPP14Lexer(inputStream);
         const tokenStream = new CommonTokenStream(lexer);
         const parser = new CPP14Parser(tokenStream);
-        const visitor = new OrToolsVisit();
         const tree = parser.translationUnit();
+        const visitor = new OrToolsVisit();
         visitor.visit(tree);
     }
 
