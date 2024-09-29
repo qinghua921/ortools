db8296b062214e45d91fc83592c6c9a88d0cb161
chendaliang <piaoyaocdl@163.com>
2024-09-30 00:22:52 +0800

    1

--- gen/gen.ts
+++ gen/gen.ts
@@ -2,54 +2,8 @@ import fs from "fs";
 import YAML from 'yaml'
 import { CharStream, CommonTokenStream } from "antlr4ng";
 import { CPP14Lexer } from "./.antlr/CPP14Lexer";
+import { ClassHeadContext, ClassSpecifierContext, CPP14Parser, NamespaceDefinitionContext } from "./.antlr/CPP14Parser";
 import { CPP14ParserVisitor } from "./.antlr/CPP14ParserVisitor";
-import { ClassNameContext, ClassSpecifierContext, CPP14Parser, NamespaceDefinitionContext } from "./.antlr/CPP14Parser";
-
-class Info
-{
-    gen_config!: GenConfig;
-}
-
-class MyVisitor extends CPP14ParserVisitor<Info>
-{
-    visitNamespaceDefinition = (ctx: NamespaceDefinitionContext) =>
-    {
-        let identifier = ctx.Identifier();
-        if (identifier && info.gen_config.namespaces.includes(identifier.getText()))
-        {
-            console.log("namespace name: " + ctx.Identifier());
-        }
-
-        return info;
-    };
-
-    visitClassSpecifier = (ctx: ClassSpecifierContext) =>
-    {
-        let classHead = ctx.memberSpecification();
-        console.log('eeeeeeeeeeeeeeee');
-        
-        console.log("class name: " + classHead?.getText());
-        
-        if (classHead)
-        {
-            console.log("class name: " + classHead.getText());
-        }
-
-        return info;
-    }
-
-    visitClassName = (ctx: ClassNameContext) =>
-    {
-        let className = ctx.Identifier()
-        console.log("class name: " + className?.getText());
-        if (className)
-        {
-            console.log("class name: " + className.getText());
-        }
-
-        return info;
-    }
-}
 
 interface GenConfig
 {
@@ -57,7 +11,79 @@ interface GenConfig
     namespaces: string[];
 }
 
+class ClassInfo
+{
+    name: string;
+    namespace: string;
+
+    constructor(name: string, namespace: string)
+    {
+        this.name = name;
+        this.namespace = namespace;
+    }
+}
+
+class NamespaceInfo
+{
+    name: string;
+    classes: Map<string, ClassInfo> = new Map();
+
+    constructor(name: string)
+    {
+        this.name = name;
+    }
+}
+
+class Info
+{
+    gen_config!: GenConfig;
+    namespace_info: Map<string, NamespaceInfo> = new Map();
+    current_namespace: string = "";
+}
+
 const info = new Info();
+
+class OrToolsVisit extends CPP14ParserVisitor<void>
+{
+    visitNamespaceDefinition = (ctx: NamespaceDefinitionContext) =>
+    {
+        let identifier = ctx.Identifier();
+        if (!identifier)
+        {
+            this.visitChildren(ctx);
+            return;
+        }
+        if (!info.gen_config.namespaces.includes(identifier.getText()))
+        {
+            this.visitChildren(ctx);
+            return;
+        }
+
+        info.current_namespace = identifier.getText();
+        if (!info.namespace_info.has(info.current_namespace))
+        {
+            info.namespace_info.set(info.current_namespace, new NamespaceInfo(info.current_namespace));
+        }
+
+        this.visitChildren(ctx);
+    };
+
+    visitClassSpecifier = (ctx: ClassSpecifierContext) =>
+    {
+        let className = ctx.classHead()?.classHeadName()?.className();
+
+        if (!className)
+        {
+            this.visitChildren(ctx);
+            return;
+        }
+
+        let classInfo = new ClassInfo(className.getText(), info.current_namespace);
+        info.namespace_info.get(info.current_namespace)!.classes.set(classInfo.name, classInfo);
+
+        this.visitChildren(ctx);
+    }
+}
 
 /***********************************************************************************
  * Script entry point.
@@ -76,12 +102,15 @@ function main()
         const lexer = new CPP14Lexer(inputStream);
         const tokenStream = new CommonTokenStream(lexer);
         const parser = new CPP14Parser(tokenStream);
-
-        const visitor = new MyVisitor();
-        visitor.visit(parser.translationUnit());
+        const visitor = new OrToolsVisit();
+        const tree = parser.translationUnit();
+        visitor.visit(tree);
     }
 
     gen_config.files.forEach(visitFile)
+
+    console.log(JSON.stringify(info, null, 2));
+
 }
 
 main()
